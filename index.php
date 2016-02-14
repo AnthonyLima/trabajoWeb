b016e2fb1d8e8701c1a166d875702e5652bdde11
AnthonyLima <a.lima.quispe@isur.edu.pe>
2016-02-14 10:12:41 -0500

    segundo commit
    agragando apariencia.css en estilo, se mejoro ademas el index
    distribuyendo en una tabla se piensa mejorar para que cada zona llame a
    un php distinto.

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <html>
 <head>
   <title>pagina web</title>
+  <link rel="stylesheet" href="estilo/principal.css">
 </head>
 <body>
-  <?php
-     print("hola desde php"); 
-  ?>
+<table border="1">
+  <tr><td colspan="2">titulo</td></tr>
+  <tr><td colspan="3">buscador</td></tr>
+  <tr><td width="20%">colIzq</td><td colspan="2">contenidos</td></tr>
+</table>
 </body>
 </html>
